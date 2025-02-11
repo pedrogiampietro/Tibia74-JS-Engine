4cac2f1e42f69ec811e2aae1316a677f59e0945f
Pedro Giampietro <pedro.hreis@yduqs.com.br>
2025-02-11 19:38:28 -0300

    add: ajusts in command to add level skill

--- src/account-database.js
+++ src/account-database.js
@@ -7,8 +7,7 @@ const bcrypt = require("bcryptjs");
 const fs = require("fs");
 const sqlite3 = require("sqlite3");
 
-const AccountDatabase = function(filepath) {
-
+const AccountDatabase = function (filepath) {
   /*
    * Class AccountDatabase
    * Wrapper for the account database
@@ -27,8 +26,7 @@ const AccountDatabase = function(filepath) {
 
   // Open the database
   this.__open(this.__handleOpen.bind(this));
-
-}
+};
 
 AccountDatabase.prototype.STATUS = new Enum(
   "OPENING",
@@ -37,88 +35,93 @@ AccountDatabase.prototype.STATUS = new Enum(
   "CLOSED"
 );
 
-AccountDatabase.prototype.__handleOpen = function(error) {
-
-  if(error) {
+AccountDatabase.prototype.__handleOpen = function (error) {
+  if (error) {
     return console.error("Error opening the database %s".format(this.filepath));
   }
 
   this.__status = this.STATUS.OPEN;
-  console.log("The sqlite database connection to %s has been opened".format(this.filepath));
+  console.log(
+    "The sqlite database connection to %s has been opened".format(this.filepath)
+  );
+};
 
-}
-
-AccountDatabase.prototype.__open = function(callback) {
-
+AccountDatabase.prototype.__open = function (callback) {
   /*
    * AccountDatabase.__open
    * Opens the sqlite3 database from file
    */
 
   // The database file already exists
-  if(fs.existsSync(this.filepath)) {
-    return this.db = new sqlite3.Database(this.filepath, sqlite3.OPEN_READWRITE, callback);
+  if (fs.existsSync(this.filepath)) {
+    return (this.db = new sqlite3.Database(
+      this.filepath,
+      sqlite3.OPEN_READWRITE,
+      callback
+    ));
   }
 
   // Create a new database
   return this.__createNewDatabase(callback);
+};
 
-}
-
-AccountDatabase.prototype.__createNewDatabase = function(callback) {
-
+AccountDatabase.prototype.__createNewDatabase = function (callback) {
   /*
    * Function AccountDatabase.__createNewDatabase
    * Creates a brand new Sqlite3 database and inserts an optional default character
    */
 
   // Create a new database
-  this.db = new sqlite3.Database(this.filepath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, callback);
+  this.db = new sqlite3.Database(
+    this.filepath,
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    callback
+  );
 
   // Serialize the control flow
-  this.db.serialize(function() {
+  this.db.serialize(
+    function () {
+      // Table that holds the accounts
+      this.__createAccountDatabase();
 
-    // Table that holds the accounts
-    this.__createAccountDatabase();
+      // Create a default character
+      if (CONFIG.DATABASE.DEFAULT_CHARACTER.ENABLED) {
+        this.__createDefaultCharacter(CONFIG.DATABASE.DEFAULT_CHARACTER);
+      }
+    }.bind(this)
+  );
+};
 
-    // Create a default character
-    if(CONFIG.DATABASE.DEFAULT_CHARACTER.ENABLED) {
-      this.__createDefaultCharacter(CONFIG.DATABASE.DEFAULT_CHARACTER);
-    }
-
-  }.bind(this));
-
-}
-
-AccountDatabase.prototype.__createDefaultCharacter = function(DEFAULT_CHARACTER) {
-
+AccountDatabase.prototype.__createDefaultCharacter = function (
+  DEFAULT_CHARACTER
+) {
   /*
    * Function AccountDatabase.__createDefaultCharacter
    * Creates and writes the configured default character to the database
    */
 
   let queryObject = new Object({
-    "account": DEFAULT_CHARACTER.ACCOUNT,
-    "password": DEFAULT_CHARACTER.PASSWORD,
-    "name": DEFAULT_CHARACTER.NAME,
-    "sex": DEFAULT_CHARACTER.SEX
-  })
-
-  // Add a default account
-  this.createAccount(queryObject, function(error) {
-
-    if(error !== null) {
-      return console.error("Error creating default character: %s".format(error));
-    }
-
-    console.log("Default character %s has been created".format(queryObject.name));
-
+    account: DEFAULT_CHARACTER.ACCOUNT,
+    password: DEFAULT_CHARACTER.PASSWORD,
+    name: DEFAULT_CHARACTER.NAME,
+    sex: DEFAULT_CHARACTER.SEX,
   });
 
-}
+  // Add a default account
+  this.createAccount(queryObject, function (error) {
+    if (error !== null) {
+      return console.error(
+        "Error creating default character: %s".format(error)
+      );
+    }
 
-AccountDatabase.prototype.__createAccountDatabase = function() {
+    console.log(
+      "Default character %s has been created".format(queryObject.name)
+    );
+  });
+};
 
+AccountDatabase.prototype.__createAccountDatabase = function () {
   /*
    * AccountDatabase.__createAccountDatabase
    * Inserts the table schema
@@ -134,23 +137,19 @@ AccountDatabase.prototype.__createAccountDatabase = function() {
       'name' VARCHAR(32) NOT NULL,
       'character' JSON NOT NULL,
       UNIQUE(account, name)
-    );`
+    );`;
 
   // Run the table creation query
-  this.db.run(tableQuery, function(error) {
-
-    if(error !== null) { 
+  this.db.run(tableQuery, function (error) {
+    if (error !== null) {
       return console.error("Error creating account table");
     }
 
     console.log("Created account table");
-
   });
+};
 
-}
-
-AccountDatabase.prototype.close = function() {
-
+AccountDatabase.prototype.close = function () {
   /*
    * Function AccountDatabase.close
    * Closes the sqlite3 database
@@ -158,22 +157,18 @@ AccountDatabase.prototype.close = function() {
 
   this.__status = this.STATUS.CLOSING;
   this.db.close(this.__handleClose.bind(this));
+};
 
-}
+AccountDatabase.prototype.__handleClose = function (error) {
+  if (error) {
+    return console.error(error.message);
+  }
 
-AccountDatabase.prototype.__handleClose = function(error) {
+  this.__status = this.STATUS.CLOSED;
+  console.log("The database connection has been closed.");
+};
 
-    if(error) {
-      return console.error(error.message);
-    }
-
-    this.__status = this.STATUS.CLOSED;
-    console.log("The database connection has been closed.");
-
-}
-
-AccountDatabase.prototype.createAccount = function(queryObject, callback) {
-
+AccountDatabase.prototype.createAccount = function (queryObject, callback) {
   /*
    * Function AccountDatabase.createAccount
    * Closes the sqlite3 database
@@ -182,42 +177,47 @@ AccountDatabase.prototype.createAccount = function(queryObject, callback) {
   const SALT_ROUNDS = 12;
 
   // Preliminary check if account already exists
-  this.getAccountCredentials(queryObject.account, function(error, result) {
-
-    if(error !== null) {
-      return callback(500, null);
-    }
-
-    // Already exists
-    if(result !== undefined) {
-      return callback(409, null);
-    }
-
-    // Slow hashing algorithm
-    bcrypt.hash(queryObject.password, SALT_ROUNDS, function(error, hash) {
-
-      // Server error if something is wrong with bcrypt
-      if(error) {
+  this.getAccountCredentials(
+    queryObject.account,
+    function (error, result) {
+      if (error !== null) {
         return callback(500, null);
       }
 
-      // Creates a new character from a blueprint
-      let account = queryObject.account.toLowerCase();
-      let name = queryObject.name.capitalize();
-      let character = this.characterCreator.create(name, queryObject.sex);
-      let values = new Array(account, hash, name, character);
+      // Already exists
+      if (result !== undefined) {
+        return callback(409, null);
+      }
 
-      // Insert in to the database (may conflict)
-      this.db.run("INSERT INTO accounts(account, hash, name, character) VALUES(?, ?, ?, ?)", values, callback);
+      // Slow hashing algorithm
+      bcrypt.hash(
+        queryObject.password,
+        SALT_ROUNDS,
+        function (error, hash) {
+          // Server error if something is wrong with bcrypt
+          if (error) {
+            return callback(500, null);
+          }
 
-    }.bind(this));
+          // Creates a new character from a blueprint
+          let account = queryObject.account.toLowerCase();
+          let name = queryObject.name.capitalize();
+          let character = this.characterCreator.create(name, queryObject.sex);
+          let values = new Array(account, hash, name, character);
 
-  }.bind(this));
+          // Insert in to the database (may conflict)
+          this.db.run(
+            "INSERT INTO accounts(account, hash, name, character) VALUES(?, ?, ?, ?)",
+            values,
+            callback
+          );
+        }.bind(this)
+      );
+    }.bind(this)
+  );
+};
 
-}
-
-AccountDatabase.prototype.saveCharacter = function(gameSocket, callback) {
-
+AccountDatabase.prototype.saveCharacter = function (gameSocket, callback) {
   /*
    * Function AccountDatabase.saveCharacter
    * Returns the character for a specific account
@@ -226,30 +226,41 @@ AccountDatabase.prototype.saveCharacter = function(gameSocket, callback) {
   // Serialize the player character
   let character = JSON.stringify(gameSocket.player);
 
-  this.db.run("UPDATE accounts SET character = ? WHERE account = ?", [character, gameSocket.account], callback);
+  this.db.run(
+    "UPDATE accounts SET character = ? WHERE account = ?",
+    [character, gameSocket.account],
+    callback
+  );
+};
 
-}
-
-AccountDatabase.prototype.getCharacter = function(account, callback) {
-
+AccountDatabase.prototype.getCharacter = function (account, callback) {
   /*
    * Function AccountDatabase.getCharacter
    * Returns the character for a specific account
    */
 
-  this.db.get("SELECT character FROM accounts WHERE account = ?", [account], callback);
+  this.db.get(
+    "SELECT character FROM accounts WHERE account = ?",
+    [account],
+    callback
+  );
+};
 
-}
-
-AccountDatabase.prototype.getAccountCredentials = function(account, callback) {
-
+AccountDatabase.prototype.getAccountCredentials = function (account, callback) {
   /*
    * Function AccountDatabase.getAccountCredentials
    * Returns the account credentials for a given account to verify the password
    */
 
-  this.db.get("SELECT hash FROM accounts WHERE account = ?", [account], callback);
+  this.db.get(
+    "SELECT hash FROM accounts WHERE account = ?",
+    [account],
+    callback
+  );
+};
 
-}
+AccountDatabase.prototype.loadCharacter = function (name) {
+  // Este método provavelmente não existe, precisamos ver como o projeto carrega os dados
+};
 
 module.exports = AccountDatabase;
--- src/command-handler.js
+++ src/command-handler.js
@@ -3,88 +3,173 @@
 const Position = requireModule("position");
 const { ServerMessagePacket } = requireModule("protocol");
 
-const CommandHandler = function() {
-
-}
+const CommandHandler = function () {};
 
 CommandHandler.prototype.WAYPOINTS = new Object({
-  "rookgaard": new Position(32097, 32219, 8),
-  "thais": new Position(32369, 32241, 8),
-  "carlin": new Position(32360, 31782, 8),
+  rookgaard: new Position(32097, 32219, 8),
+  thais: new Position(32369, 32241, 8),
+  carlin: new Position(32360, 31782, 8),
   "ab'dendriel": new Position(32732, 31634, 8),
-  "venore": new Position(32957, 32076, 8),
-  "poh": new Position(32816, 32260, 10),
+  venore: new Position(32957, 32076, 8),
+  poh: new Position(32816, 32260, 10),
   "gm-island": new Position(32316, 31942, 8),
-  "senja": new Position(32125, 31667, 8),
-  "dracona": new Position(32804, 31586, 15),
+  senja: new Position(32125, 31667, 8),
+  dracona: new Position(32804, 31586, 15),
   "orc-fortress": new Position(32882, 31772, 9),
-  "edron": new Position(33217, 31814, 7),
-  "kazordoon": new Position(32649, 31925, 4),
-  "ankrahmun": new Position(33194, 32853, 7),
-  "darama": new Position(33213, 32454, 14),
-  "cormaya": new Position(33301, 31968, 8),
-  "fibula": new Position(32174, 32437, 8),
+  edron: new Position(33217, 31814, 7),
+  kazordoon: new Position(32649, 31925, 4),
+  ankrahmun: new Position(33194, 32853, 7),
+  darama: new Position(33213, 32454, 14),
+  cormaya: new Position(33301, 31968, 8),
+  fibula: new Position(32174, 32437, 8),
   "white-flower": new Position(32346, 32362, 9),
   "femur-hills": new Position(32536, 31837, 11),
   "ghost-ship": new Position(33321, 32181, 8),
-  "mintwallin": new Position(32456, 32100, 0),
-  "cyclopolis": new Position(33251, 31695, 8),
-  "annihilator": new Position(33221, 31671, 2)
+  mintwallin: new Position(32456, 32100, 0),
+  cyclopolis: new Position(33251, 31695, 8),
+  annihilator: new Position(33221, 31671, 2),
 });
 
-CommandHandler.prototype.handleCommandWaypoint = function(player, waypoint) {
-
+CommandHandler.prototype.handleCommandWaypoint = function (player, waypoint) {
   /*
    * CommandHandler.handleCommandWaypoint
    * Executes the waypoint command
    */
 
-  if(!this.WAYPOINTS.hasOwnProperty(waypoint)) {
+  if (!this.WAYPOINTS.hasOwnProperty(waypoint)) {
     return player.sendCancelMessage("This waypoint does not exist.");
   }
 
-  return gameServer.world.creatureHandler.teleportCreature(player, this.WAYPOINTS[waypoint]);
+  return gameServer.world.creatureHandler.teleportCreature(
+    player,
+    this.WAYPOINTS[waypoint]
+  );
+};
 
-}
+CommandHandler.prototype.handleCommandAddSkill = function (
+  player,
+  skill,
+  amount
+) {
+  if (skill === "level") {
+    try {
+      const skills = player.skills;
+      if (!skills) {
+        console.error("[AddSkill] Could not find skills object");
+        return gameServer.world.broadcastPacket(
+          new ServerMessagePacket("Could not access player skills")
+        );
+      }
 
-CommandHandler.prototype.handle = function(player, message) {
+      // Obter exp atual do objeto skills
+      const currentLevel = skills[7] || 1;
+      const currentExp = skills[6] || 0;
+      const targetLevel = currentLevel + Number(amount);
 
+      console.log("[AddSkill] Current Level:", currentLevel);
+      console.log("[AddSkill] Current Exp:", currentExp);
+      console.log("[AddSkill] Target Level:", targetLevel);
+
+      // Calcular exp necessária
+      const Skill = requireModule("skill");
+      const skillInstance = new Skill();
+      const targetExp = skillInstance.getExperience(targetLevel);
+      const currentLevelExp = skillInstance.getExperience(currentLevel);
+      const expRequired = targetExp - currentLevelExp;
+
+      console.log("[AddSkill] Required Exp:", expRequired);
+
+      // Atualizar exp no objeto skills
+      skills[6] = currentExp + expRequired;
+
+      // Recalcular atributos baseados no novo level
+      const newHealth = 150 + (targetLevel - 1) * 5; // 5 HP por level
+      const newMana = 35 + (targetLevel - 1) * 5; // 5 MP por level
+      const newCap = 400 + (targetLevel - 1) * 10; // 10 CAP por level
+
+      // Atualizar o player em tempo real
+      player.setProperty(CONST.PROPERTIES.HEALTH, newHealth);
+      player.setProperty(CONST.PROPERTIES.MAX_HEALTH, newHealth);
+      player.setProperty(CONST.PROPERTIES.MANA, newMana);
+      player.setProperty(CONST.PROPERTIES.MAX_MANA, newMana);
+      player.setProperty(CONST.PROPERTIES.CAPACITY, newCap);
+
+      // Notificar o cliente sobre as mudanças
+      gameServer.world.broadcastPacket(
+        new ServerMessagePacket(
+          `Added ${expRequired} experience points (${amount} levels). New level: ${targetLevel}`
+        )
+      );
+
+      // O servidor deve ter um mecanismo próprio para persistir os dados
+      // quando o player deslogar ou em intervalos regulares
+
+      return true;
+    } catch (error) {
+      console.error("[AddSkill] Error:", error);
+      return gameServer.world.broadcastPacket(
+        new ServerMessagePacket("An error occurred while adding experience.")
+      );
+    }
+  }
+
+  console.log("[AddSkill] Invalid skill type:", skill);
+  return gameServer.world.broadcastPacket(
+    new ServerMessagePacket("Invalid skill type. Available: level")
+  );
+};
+
+CommandHandler.prototype.handle = function (player, message) {
   //if(player.getProperty(CONST.PROPERTIES.ROLE) !== CONST.ROLES.ADMIN) {
   //  return;
   //}
 
   message = message.split(" ");
 
-  if(message[0] === "/property") {
+  if (message[0] === "/property") {
     return player.setProperty(Number(message[1]), Number(message[2]));
   }
 
-  if(message[0] === "/waypoint") {
+  if (message[0] === "/waypoint") {
     return this.handleCommandWaypoint(player, message[1]);
   }
 
-  if(message[0] === "/teleport") {
-    return gameServer.world.creatureHandler.teleportCreature(player, new Position(Number(message[1]), Number(message[2]), Number(message[3])));
+  if (message[0] === "/teleport") {
+    return gameServer.world.creatureHandler.teleportCreature(
+      player,
+      new Position(Number(message[1]), Number(message[2]), Number(message[3]))
+    );
   }
 
-  if(message[0] === "/broadcast") {
-    return gameServer.world.broadcastPacket(new ServerMessagePacket(message[1]));
+  if (message[0] === "/broadcast") {
+    return gameServer.world.broadcastPacket(
+      new ServerMessagePacket(message[1])
+    );
   }
 
-  if(message[0] === "/spawn") {
+  if (message[0] === "/spawn") {
     let id = Number(message[1]);
-    return gameServer.world.creatureHandler.spawnCreature(id, player.getPosition());
+    return gameServer.world.creatureHandler.spawnCreature(
+      id,
+      player.getPosition()
+    );
   }
 
-  if(message[0] === "/path") {
+  if (message[0] === "/path") {
     let a = player.getPosition();
     let b = a.add(new Position(Number(message[1]), Number(message[2]), 0));
     let p = gameServer.world.findPath(player, a, b, 1);
-    p.forEach(function(tile) {
-      gameServer.world.sendMagicEffect(tile.getPosition(), CONST.EFFECT.MAGIC.TELEPORT);
+    p.forEach(function (tile) {
+      gameServer.world.sendMagicEffect(
+        tile.getPosition(),
+        CONST.EFFECT.MAGIC.TELEPORT
+      );
     });
   }
 
-}
+  if (message[0] === "/addskill") {
+    return this.handleCommandAddSkill(player, message[1], message[2]);
+  }
+};
 
 module.exports = CommandHandler;
